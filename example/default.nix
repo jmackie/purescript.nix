4e8ecbca52acd4311404bf05279ba75be669e220
Jordan Mackie <jmackie@protonmail.com>
2019-04-09 20:25:39 +0100

    Update example to import remote repo

--- example/default.nix
+++ example/default.nix
@@ -1,6 +1,11 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
-  purescript = import ../. {};
+  purescript = import (nixpkgs.fetchFromGitHub {
+    owner = "jmackie";
+    repo = "purescript-nix";
+    rev = "2d87289f22bc0f823033fb59c16df6009de09038";
+    sha256 = "1ia36ssl642nkmnqg4iqz2jmmgnl7n3zcb3hrmq3ybgnv81ygr1k";
+  }) { inherit nixpkgs; } ;
 in
 purescript.compile {
   name = "example";
