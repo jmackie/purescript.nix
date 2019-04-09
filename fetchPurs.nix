03083c39259e5e120a227108337a20df047f4a67
Jordan Mackie <jmackie@protonmail.com>
2019-04-09 20:35:59 +0100

    Inline some stuff

--- fetchPurs.nix
+++ fetchPurs.nix
@@ -1,16 +1,5 @@
 { nixpkgs, version, url, sha256 }:
 
-let
-  dynamic-linker =
-    nixpkgs.stdenv.cc.bintools.dynamicLinker;
-
-  patchelf = libPath:
-    if nixpkgs.stdenv.isDarwin then "" else ''
-      chmod u+w $PURS
-      patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
-      chmod u-w $PURS
-    '';
-in
 nixpkgs.stdenv.mkDerivation rec {
   name = "purs";
   inherit version;
@@ -26,6 +15,11 @@ nixpkgs.stdenv.mkDerivation rec {
     mkdir -p $out/bin
     PURS="$out/bin/purs"
     install -D -m555 -T purs $PURS
-    ${patchelf libPath}
+
+    ${if nixpkgs.stdenv.isDarwin then "" else ''
+    chmod u+w $PURS
+    patchelf --interpreter ${nixpkgs.stdenv.cc.bintools.dynamicLinker} --set-rpath ${libPath} $PURS
+    chmod u-w $PURS
+    ''}
   '';
 }
