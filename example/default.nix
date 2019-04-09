75cbc335598e99b32615e147d88c312412fea056
Jordan Mackie <jmackie@protonmail.com>
2019-04-09 20:37:33 +0100

    Bump example repo version
    This could get annoying...

--- example/default.nix
+++ example/default.nix
@@ -3,8 +3,8 @@ let
   purescript = import (nixpkgs.fetchFromGitHub {
     owner = "jmackie";
     repo = "purescript-nix";
-    rev = "2d87289f22bc0f823033fb59c16df6009de09038";
-    sha256 = "1ia36ssl642nkmnqg4iqz2jmmgnl7n3zcb3hrmq3ybgnv81ygr1k";
+    rev = "03083c39259e5e120a227108337a20df047f4a67";
+    sha256 = "0hsmhj6ivy74a7arwrviwszllccz4fkycgvfr7clj733jsjjzir2";
   }) { inherit nixpkgs; } ;
 in
 purescript.compile {
