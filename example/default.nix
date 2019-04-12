3f5cda06f47c6f9f4a0494f4cb38ec028f91e3e2
Jordan Mackie <jordan.mackie@habito.com>
2019-04-12 09:48:18 +0100

    Link the example to the parent, rather than keep updating the commit

--- example/default.nix
+++ example/default.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  purescript = import (pkgs.fetchFromGitHub {
-    owner = "jmackie";
-    repo = "purescript-nix";
-    rev = "03083c39259e5e120a227108337a20df047f4a67";
-    sha256 = "0hsmhj6ivy74a7arwrviwszllccz4fkycgvfr7clj733jsjjzir2";
-  }) { inherit pkgs; } ;
+  purescript = import ./.. { inherit pkgs; };
+  # purescript = import (pkgs.fetchFromGitHub {
+  #   owner = "jmackie";
+  #   repo = "purescript-nix";
+  #   rev = "COMMIT";
+  #   sha256 = "HASH";
+  # }) { inherit pkgs; } ;
 in
 purescript.compile {
   name = "example";
