80b230cd50c2889e8d4de1b2a183d71dd6eb0139
Jordan Mackie <jmackie@protonmail.com>
2019-04-27 19:25:05 +0100

    Cleanup

--- elaborator/README.md
+++ elaborator/README.md
@@ -2,3 +2,23 @@
 cabal new-run elaborator -- --help
 cabal new-run elaborator -- <(curl https://raw.githubusercontent.com/purescript/package-sets/master/packages.json)
 ```
+
+## Developing
+
+You probably want to use the provided Nix shell
+
+```bash
+nix-shell
+```
+
+Once in the shell, there's a convenient `ghcid` wrapper
+
+```bash
+ghcid_
+```
+
+Or more succinctly
+
+```bash
+nix-shell --run ghcid_
+```
---
+++ elaborator/default.nix
@@ -0,0 +1,43 @@
+{ pkgs ? import <nixpkgs> {}
+, compiler ? "ghc864"
+}:
+let
+  haskellPackages =
+    pkgs.haskell.packages.${compiler};
+
+  filterEnvironmentFiles =
+    builtins.filterSource (path: type: !(pkgs.lib.strings.hasPrefix ".ghc.environment" (baseNameOf path)));
+
+  # ghcid with support for cabal new-repl
+  ghcid_ =
+    let newRepl = ''
+      cabal new-repl \
+        --ghc-options=-fno-code \
+        --ghc-options=-fno-break-on-exception \
+        --ghc-options=-fno-break-on-error \
+        --ghc-options=-v1 \
+        --ghc-options=-ferror-spans \
+        --ghc-options=-j
+    '';
+    in pkgs.writeShellScriptBin "ghcid_" ''
+      ${haskellPackages.ghcid}/bin/ghcid \
+        --restart elaborator.cabal \
+        --command "${newRepl}" \
+        --setup ":l Main"
+  '';
+
+  drv =
+    haskellPackages.callCabal2nix "elaborator" (filterEnvironmentFiles ./.) {};
+
+  env = haskellPackages.shellFor {
+    packages = p: [ drv ];
+    buildInputs = [
+      haskellPackages.ghcid ghcid_
+      haskellPackages.stylish-haskell
+    ];
+  };
+in
+if pkgs.lib.inNixShell then env else drv
+
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/generic-builder.nix
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/make-package-set.nix
